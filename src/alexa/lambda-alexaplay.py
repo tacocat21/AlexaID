300452f76e90ce47ae99deb96ea935009aaef987
xun2 <xun2@illinois.edu>
2017-01-22 00:25:28 -0500

    lambda template for alexa

--- src/alexa/lambda-alexaplay.py
+++ src/alexa/lambda-alexaplay.py
@@ -6,6 +6,7 @@ as testing instructions are located at http://amzn.to/1LzFrj6
 For additional samples, visit the Alexa Skills Kit Getting Started guide at
 http://amzn.to/1LGWsLG
 """
+ 
 
 from __future__ import print_function
 import requests
@@ -77,6 +78,24 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
+def openthedoor(intent, session):
+
+    card_title = "Opening the door"
+    session_attributes = {}
+    should_end_session = False
+
+    
+
+
+    speech_output = ""
+    reprompt_text = "You can ask me to play a song by saying, " \
+                        "Play Closer"
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+    
+"""
 def playsong(intent, session):
 
     card_title = "playing song"
@@ -106,7 +125,7 @@ def start(intent, session):
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
-
+"""
 
 # --------------- Events ------------------
 
@@ -138,10 +157,8 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "PlaySong":
-        return playsong(intent, session)
-    elif intent_name == "Start":
-        return start(intent, session)
+    if intent_name == "OpenTheDoor":
+        return openthedoor(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
